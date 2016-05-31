dbc70707cb0d45c2127257703d3c6cae910a6a3d
Sebastian Dittrich <Sebastian Dittrich>
2016-05-31 19:23:27 +0200

    Show Offer

--- app/src/main/java/com/hska/ebusiness/toolbar/dao/ToolbarDBHelper.java
+++ app/src/main/java/com/hska/ebusiness/toolbar/dao/ToolbarDBHelper.java
@@ -19,7 +19,6 @@ import static com.hska.ebusiness.toolbar.dao.DatabaseSchema.RentalEntry;
 import static com.hska.ebusiness.toolbar.dao.DatabaseSchema.UserEntry;
 
 public class ToolbarDBHelper extends SQLiteOpenHelper {
-
     public static final int DATABASE_VERSION = 8;
     public static final String DATABASE_NAME = "toolbar.db";
 
