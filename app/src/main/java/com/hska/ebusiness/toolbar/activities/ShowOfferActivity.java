6724041ab98dd70799ce66d83ccf3464656d08d1
Sebastian Dittrich <Sebastian Dittrich>
2016-06-07 08:57:43 +0200

    Commit

--- app/src/main/java/com/hska/ebusiness/toolbar/activities/ShowOfferActivity.java
+++ app/src/main/java/com/hska/ebusiness/toolbar/activities/ShowOfferActivity.java
@@ -2,11 +2,15 @@ package com.hska.ebusiness.toolbar.activities;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +27,11 @@ import com.hska.ebusiness.toolbar.util.RentalMapper;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 
+import android.net.Uri;
+
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +46,7 @@ public class ShowOfferActivity extends AppCompatActivity {
 
     private Offer offer;
     private User user;
+    private ImageView offerImage;
 
     private final String TAG = this.getClass().getSimpleName();
 
@@ -54,25 +64,44 @@ public class ShowOfferActivity extends AppCompatActivity {
     }
 
     private void initContent() {
-        ((TextView) findViewById(R.id.show_input_offer_name)).setText(offer.getName());
-        ((TextView) findViewById(R.id.show_input_offer_price)).setText(String.valueOf(offer.getPrice()));
-        ((TextView) findViewById(R.id.show_input_offer_description)).setText(offer.getDescription());
+        if(offer.getName() != null)
+            ((TextView) findViewById(R.id.show_input_offer_name)).setText(offer.getName());
+        if(offer.getPrice() > 0)
+            ((TextView) findViewById(R.id.show_input_offer_price)).setText(String.valueOf(offer.getPrice()));
+        if(offer.getDescription() != null)
+            ((TextView) findViewById(R.id.show_input_offer_description)).setText(offer.getDescription());
+
+        if(offer.getImage() != null) {
+            offerImage = (ImageView) findViewById(R.id.image_offer_image);
+
+            final Uri image = Uri.parse(offer.getImage());
+            if (image != null && new File(image.getPath()).exists()) {
+                try {
+                    offerImage.setImageBitmap(this.resizeImage(image));
+                } catch (IOException e) {
+                    Log.e(TAG, "Error while initializing image: " + e.getMessage());
+                    return;
+                }
+            }
+        } else {
+            offerImage.setImageResource(R.drawable.ic_insert_photo_black_48dp);
+        }
+
 
         MCalendarView calendarView = ((MCalendarView) findViewById(R.id.calendar));
 
-        for(Rental rental : getRentals(offer.getId())){
+        if(getRentals(offer.getId()) != null) {
+            for (Rental rental : getRentals(offer.getId())) {
+                if (rental.getStatus() == 1) {
+                    DateTime fromDate = new DateTime(rental.getRentFrom());
+                    DateTime toDate = new DateTime(rental.getRentTo());
 
-            if(rental.getStatus() == 1){
-
-                DateTime fromDate = new DateTime(rental.getRentFrom());
-                DateTime toDate = new DateTime(rental.getRentTo());
-
-                for(DateTime date : getRentalDays(fromDate, toDate)) {
-                    calendarView.markDate(date.getYear(), date.getMonthOfYear(), date.getDayOfMonth());
-                }
+                    for (DateTime date : getRentalDays(fromDate, toDate)) {
+                        calendarView.markDate(date.getYear(), date.getMonthOfYear(), date.getDayOfMonth());
+                    }
+                } else
+                    continue;
             }
-            else
-                continue;
         }
 
     }
@@ -139,6 +168,25 @@ public class ShowOfferActivity extends AppCompatActivity {
         }
         cursor.close();
         return null;
+    }
+
+    /**
+     * Resizes image to fit in ImageView
+     *
+     * @param file Uri to the image path
+     * @return the resized bitmap
+     * @throws IOException if access to external storage fails
+     */
+    private Bitmap resizeImage(final Uri file) throws IOException {
+        final Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), file);
+
+        final float width = bitmap.getWidth();
+        final float height = bitmap.getHeight();
+
+        final int aimedHeight = 1024;
+        final int aimedWidth = (int) (width / height * (float) aimedHeight);
+
+        return Bitmap.createScaledBitmap(bitmap, aimedWidth, aimedHeight, false);
     }
 
 }
